57e2fd3e4391721b03ea4f3d363779bcddb8e9b1
bebiksior <bebiks.priv@gmail.com>
2026-03-14 18:21:08 +0100

    Revert "fix: missing Ctrl + A keybind in HttpEditor"
    This reverts commit 2ca07e2de708ec62f2fa9a0fc1099b7a316c02b3.

--- packages/frontend/src/components/HttpEditor/extensions/index.ts
+++ packages/frontend/src/components/HttpEditor/extensions/index.ts
@@ -1,37 +1,17 @@
-import { Prec, type Extension } from "@codemirror/state";
-import { keymap, type KeyBinding } from "@codemirror/view";
+import type { Extension } from "@codemirror/state";
+import { keymap } from "@codemirror/view";
 import { contentTypeValueHint } from "./contentTypeValues";
 import { responseHeaderNameHint } from "./responseHeaderNames";
 import { statusLabelHint } from "./statusLabel";
 
 const hints = [statusLabelHint, responseHeaderNameHint, contentTypeValueHint];
 
-const selectAllInEditor: KeyBinding["run"] = (view) => {
-  view.dispatch(
-    view.state.update({
-      selection: {
-        anchor: 0,
-        head: view.state.doc.length,
-      },
-      userEvent: "select",
-    }),
-  );
-  return true;
-};
-
-export const httpEditorKeyBindings: KeyBinding[] = [
-  {
-    key: "Mod-a",
-    run: selectAllInEditor,
-    preventDefault: true,
-  },
-  {
-    key: "Tab",
-    run: (view) => hints.some((hint) => hint.accept(view)),
-  },
-];
-
 export const httpEditorExtensions: Extension[] = [
   ...hints.map((hint) => hint.extension),
-  Prec.high(keymap.of(httpEditorKeyBindings)),
+  keymap.of([
+    {
+      key: "Tab",
+      run: (view) => hints.some((hint) => hint.accept(view)),
+    },
+  ]),
 ];
